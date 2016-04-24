6612c241ca422b85b80ec7f645bad173930ada71
Felipe Frizzo <felipefrizzo@gmail.com>
2016-04-24 15:35:39 -0300

    Implementation DAO save

--- src/br/univel/generics/ClientDaoImplatation.java
+++ src/br/univel/generics/ClientDaoImplatation.java
@@ -1,16 +1,41 @@
 package br.univel.generics;
 
+import br.univel.database.ConnectionDB;
 import br.univel.model.Client;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
  * Created by felipefrizzo on 4/24/16.
  */
 public class ClientDaoImplatation implements Dao<Client,Integer> {
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
+    private Connection con = ConnectionDB.getInstance().open();
+    private Execute ex = new Execute();
+    private List<Client> list = null;
+    
     @Override
     public void save(Client client) {
+        try {
+            ps = ex.getSqlInsert(con, client);
+            ps.executeUpdate();
+            ps.close();
 
+            System.out.println("Cliente cadastrado com sucesso!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
